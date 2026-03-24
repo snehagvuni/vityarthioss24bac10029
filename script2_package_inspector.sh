62a15d12e57d30391fa68883bf0430ec48bb7bf0
snehagvuni <sneha.24bac10029@vitbhopal.ac.in>
2026-03-24 23:59:23 +0530

    Update script2_package_inspector.sh

--- script2_package_inspector.sh
+++ script2_package_inspector.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+# Script 2: FOSS Package Inspector
+# Author: Sneha Gayatri Vogeti
 
+# Package selection
+PACKAGE="python3"
+# Check if package is installed
+if dpkg -l | grep -q $PACKAGE; then
+    echo "$PACKAGE is installed."
+    dpkg -s $PACKAGE | grep -E 'Version|Maintainer|Description'
+else
+    echo "$PACKAGE is NOT installed."
+fi
+# Package description
+case $PACKAGE in
+    python3) echo "Python: community-driven language used worldwide" ;;
+esac
